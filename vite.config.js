df213b4c3fc96e82dee97dbb57439d40fb94909c
gonencozgan <gonencozgan@gmail.com>
2025-07-25 15:32:16 +0300

    repo adını KisiselSite olarak güncelledim, base ve homepage ayarlandı

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
+const repoName = "KisiselSite";  // Yeni ad
+
 export default defineConfig({
-  base: "/KisiselSite2/",
+  base:
+    process.env.NODE_ENV === "production"
+      ? `/${repoName}/`
+      : "/",
   plugins: [react()],
   resolve: {
     alias: {
